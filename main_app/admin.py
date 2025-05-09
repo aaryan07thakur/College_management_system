6d3f2f9cb587788b2cbbf54a8d02d339f91e1f7e
Aaryan Thakur <princeaaryan06@gmail.com>
2025-05-10 01:51:03 +0545

    Initial commit of College Management System college project

---
+++ main_app/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
+# Register your models here.
+
+
+class UserModel(UserAdmin):
+    ordering = ('email',)
+
+
+admin.site.register(CustomUser, UserModel)
+admin.site.register(Staff)
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(Subject)
+admin.site.register(Session)
+admin.site.register(FeedbackStudent)
+admin.site.register(FeedbackStaff)
+admin.site.register(LeaveReportStaff)
+admin.site.register(LeaveReportStudent)
+admin.site.register(NotificationStaff)
+admin.site.register(NotificationStudent)
+admin.site.register(StudentResult)
+admin.site.register(Attendance)
+
+
